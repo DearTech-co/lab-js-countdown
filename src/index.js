e1a2b2703d9d573df054714f561ce3af78a88c58
DearTech-co <matthew@deartech.co>
2025-07-05 13:15:55 +0100

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
@@ -8,15 +9,33 @@ let timer = null; // Variable to store the interval
 
 // Your code goes here ...
 
-
+document.querySelector("#start-btn").addEventListener("click", function() {
+  console.log("Start button clicked!");
+  startCountdown();
+}
+);
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
+
   console.log("startCountdown called!");
 
-
   // Your code goes here ...
+
+  let i = 10;
+  const countDown = setInterval(() => {
+  (document.querySelector("#time").innerText = i)
+    i--;
+    document.querySelector("#start-btn").disabled = true; 
+
+    if (i < 0) {
+      clearInterval(countDown);
+      document.querySelector("#start-btn").disabled = false;
+      showToast("#toast-message");
+    }
+  }, 1000);
+
 }
 
 
@@ -28,11 +47,20 @@ function showToast(message) {
 
   // Your code goes here ...
 
+document.querySelector("#toast").classList.add("show");
+setTimeout(() => {
+    document.querySelector("#toast").classList.remove("show");
+  }, 3000);
+
+
 
 
 
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
-
+document.querySelector("#close-toast").addEventListener("click", function() {
+  document.querySelector("#toast").classList.remove("show");
+  });
 }
+
